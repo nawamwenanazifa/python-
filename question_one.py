e07e7429a4feb193c5daada508ccc21e17c6c23e
babiryesandra <143062934+babiryesandra@users.noreply.github.com>
2024-10-10 01:06:02 -0700

    solution

---
+++ question_one.py
@@ -0,0 +1,6 @@
+#question 1(a)
+# create a python programe of a triangle
+base = int(input("Enter the base of a triangle"))
+height = int(input("Enter the height of a triangle"))
+area_of_a_triangle =( 1/2*base*height)
+print(f"The area of a triangle is : {area_of_a_triangle}")
